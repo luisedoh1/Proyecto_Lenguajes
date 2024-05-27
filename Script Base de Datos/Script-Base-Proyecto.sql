480447462d4274de0d62070c335d19d4f7a26fe5
luisedoh1 <luisedoh1@gmail.com>
2024-05-26 21:47:53 -0600

    Update Script-Base-Proyecto.sql
    Correcciones en las constraints de los nombres de las PK y FK

--- Script Base de Datos/Script-Base-Proyecto.sql
+++ Script Base de Datos/Script-Base-Proyecto.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE Proyecto;
 
 USE Proyecto;
@@ -11,7 +12,7 @@ CREATE TABLE Rol (
 INSERT INTO Rol (Nombre) VALUES ('admin'), ('vendedor'), ('cliente');
 
 CREATE TABLE Usuario (
-    ID_Usuario INT PRIMARY KEY IDENTITY(1,1)
+    ID_Usuario INT IDENTITY(1,1)
 	,Nombre NVARCHAR(100) NOT NULL
 	,Email NVARCHAR(100) UNIQUE NOT NULL
 	,Contraseña NVARCHAR(255) NOT NULL
@@ -21,18 +22,20 @@ CREATE TABLE Usuario (
 );
 
 CREATE TABLE Categoria (
-    ID_Categoria INT PRIMARY KEY IDENTITY(1,1),
+    ID_Categoria INT  IDENTITY(1,1),
     Nombre NVARCHAR(100) NOT NULL,
     Descripcion NVARCHAR(MAX) NULL
+	CONSTRAINT PK_Categoria PRIMARY KEY (ID_Categoria)
 );
 
 CREATE TABLE Caracteristica (
-    ID_Caracteristica INT PRIMARY KEY IDENTITY(1,1),
+    ID_Caracteristica INT IDENTITY(1,1),
     Nombre NVARCHAR(100) NOT NULL
+	CONSTRAINT PK_Caracteristica PRIMARY KEY (ID_Caracteristica)
 );
 
 CREATE TABLE Producto (
-    ID_Producto INT PRIMARY KEY IDENTITY(1,1),
+    ID_Producto INT IDENTITY(1,1),
     Codigo NVARCHAR(50) NOT NULL,
     Nombre NVARCHAR(100) NOT NULL,
     Descripcion NVARCHAR(MAX) NULL,
@@ -42,75 +45,84 @@ CREATE TABLE Producto (
     Precio DECIMAL(10, 2) NOT NULL,
     Caracteristica_ID1 INT NOT NULL,
     Caracteristica_ID2 INT NULL,
-    FOREIGN KEY (Categoria_ID) REFERENCES Categoria(ID_Categoria),
-    FOREIGN KEY (Caracteristica_ID1) REFERENCES Caracteristica(ID_Caracteristica),
-    FOREIGN KEY (Caracteristica_ID2) REFERENCES Caracteristica(ID_Caracteristica)
+	CONSTRAINT PK_Producto PRIMARY KEY (ID_Producto),
+    CONSTRAINT FK_Categoria FOREIGN KEY (Categoria_ID) REFERENCES Categoria(ID_Categoria),
+    CONSTRAINT FK_Caracteristica_ID1 FOREIGN KEY (Caracteristica_ID1) REFERENCES Caracteristica(ID_Caracteristica),
+    CONSTRAINT FK_Caracteristica_ID2 FOREIGN KEY (Caracteristica_ID2) REFERENCES Caracteristica(ID_Caracteristica)
 );
 
 CREATE TABLE Orden (
-    ID_Orden INT PRIMARY KEY IDENTITY(1,1),
+    ID_Orden INT IDENTITY(1,1),
     ID_Usuario INT NOT NULL,
     Fecha DATETIME NOT NULL DEFAULT GETDATE(),
     Estado NVARCHAR(20) CHECK (Estado IN ('en proceso', 'enviado')) NOT NULL,
     Total DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
+	CONSTRAINT PK_Orden PRIMARY KEY (ID_Orden),
+    CONSTRAINT FK_ID_Usuario_Orden FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
 );
 
 CREATE TABLE Detalle_Orden (
-    ID_Detalle_Orden INT PRIMARY KEY IDENTITY(1,1),
+    ID_Detalle_Orden INT IDENTITY(1,1),
     ID_Orden INT NOT NULL,
     ID_Producto INT NOT NULL,
     Cantidad INT NOT NULL,
     Precio_Unitario DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (ID_Orden) REFERENCES Orden(ID_Orden),
-    FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
+	CONSTRAINT PK_Detalle_Orden PRIMARY KEY (ID_Detalle_Orden),
+    CONSTRAINT FK_ID_Orden FOREIGN KEY (ID_Orden) REFERENCES Orden(ID_Orden),
+    CONSTRAINT FK_ID_Producto_Orden FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
 );
 
 CREATE TABLE Carrito_Compra (
-    ID_Carrito INT PRIMARY KEY IDENTITY(1,1),
+    ID_Carrito INT IDENTITY(1,1),
     ID_Usuario INT NOT NULL,
     Fecha_Creacion DATETIME NOT NULL DEFAULT GETDATE(),
-    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
+	CONSTRAINT PK_Carrito PRIMARY KEY (ID_Carrito),
+    CONSTRAINT FK_ID_Usuario_Carrito FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
 );
 
 CREATE TABLE Detalle_Carrito (
-    ID_Detalle_Carrito INT PRIMARY KEY IDENTITY(1,1),
+    ID_Detalle_Carrito INT IDENTITY(1,1),
     ID_Carrito INT NOT NULL,
     ID_Producto INT NOT NULL,
-    Cantidad INT NOT NULL,
-    FOREIGN KEY (ID_Carrito) REFERENCES Carrito_Compra(ID_Carrito),
-    FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
+    Cantidad INT NOT NULL
+	CONSTRAINT PK_Detalle_Carrito PRIMARY KEY (ID_Detalle_Carrito),
+    CONSTRAINT FK_ID_Carrito FOREIGN KEY (ID_Carrito) REFERENCES Carrito_Compra(ID_Carrito),
+    CONSTRAINT FK_ID_Producto_Carrito FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
 );
 
 CREATE TABLE Direccion_Entrega (
-    ID_Direccion INT PRIMARY KEY IDENTITY(1,1),
+    ID_Direccion INT IDENTITY(1,1),
     ID_Usuario INT NOT NULL,
     Direccion NVARCHAR(MAX) NOT NULL,
     Ciudad NVARCHAR(100) NOT NULL,
     Pais NVARCHAR(100) NOT NULL,
     Codigo_Postal NVARCHAR(20) NOT NULL,
-    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
+	CONSTRAINT PK_Direccion PRIMARY KEY (ID_Direccion),
+    CONSTRAINT FK_ID_Usuario_Direccion FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
 );
 
 CREATE TABLE Tarjeta_Credito (
-    ID_Tarjeta INT PRIMARY KEY IDENTITY(1,1),
+    ID_Tarjeta INT IDENTITY(1,1),
     ID_Usuario INT NOT NULL,
     Numero_Tarjeta NVARCHAR(20) NOT NULL,
     Fecha_Expiracion DATE NOT NULL,
     Token NVARCHAR(255) NOT NULL,
-    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
+	CONSTRAINT PK_Tarjeta PRIMARY KEY (ID_Tarjeta),
+    CONSTRAINT FK_ID_Usuario_Tarjeta FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
 );
 
 CREATE TABLE Lista_Deseo (
-    ID_Lista_Deseo INT PRIMARY KEY IDENTITY(1,1),
+    ID_Lista_Deseo INT IDENTITY(1,1),
     ID_Usuario INT NOT NULL,
-    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
+	CONSTRAINT PK_Lista_Deseo PRIMARY KEY (ID_Lista_Deseo),
+    CONSTRAINT FK_ID_Usuario_ListaD FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
 );
 
 CREATE TABLE Detalle_Lista_Deseo (
-    ID_Detalle_Lista INT PRIMARY KEY IDENTITY(1,1),
+    ID_Detalle_Lista INT IDENTITY(1,1),
     ID_Lista_Deseo INT NOT NULL,
     ID_Producto INT NOT NULL,
-    FOREIGN KEY (ID_Lista_Deseo) REFERENCES Lista_Deseo(ID_Lista_Deseo),
-    FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
+	CONSTRAINT PK_Detalle_Lista PRIMARY KEY (ID_Detalle_Lista),
+    CONSTRAINT FK_ID_Lista_Deseo FOREIGN KEY (ID_Lista_Deseo) REFERENCES Lista_Deseo(ID_Lista_Deseo),
+    CONSTRAINT FK_ID_Producto_ListaD FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
 );
