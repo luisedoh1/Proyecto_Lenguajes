ae8e1adcae5b3d94c50d48061f17e56e403fae86
luisedoh1 <luisedoh1@gmail.com>
2024-07-05 15:14:50 -0600

    Arreglo de warning

--- front-end/src/components/Hero/Hero.jsx
+++ front-end/src/components/Hero/Hero.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Hero.css';
 import heroImage from '../imgs/Hero.png';
@@ -9,7 +8,7 @@ const Hero = () => {
       <div className="hero-content">
         <h1>Welcome to Guardian Tech</h1>
         <p>Find the best prices in electronics just for you</p>
-        <a className="cta-button"><Link to="/products">See More</Link></a>
+        <Link to="/products" className="cta-button">See More</Link>
       </div>
     </div>
   );
